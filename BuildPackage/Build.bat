b729f51b4c91c4cbc8fcae1b8b87577af658a328
ruicarneiro52 <40863145+ruicarneiro52@users.noreply.github.com>
2018-07-06 15:05:27 +0100

    new

---
+++ BuildPackage/Build.bat
@@ -0,0 +1,12 @@
+ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
+ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
+ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
+ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
+cd ..\UmbracoCultures\
+
+Call npm install
+Call grunt --buildversion %APPVEYOR_BUILD_VERSION% --buildbranch %APPVEYOR_REPO_BRANCH% --packagesuffix %UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
+cd ..\BuildPackage\
+Call Tools\nuget.exe restore ..\UmbracoCultures.sln
+echo yo
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj

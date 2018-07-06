36eebcf6f6315d6e8c29d7e88ea456ed85447897
ruicarneiro52 <40863145+ruicarneiro52@users.noreply.github.com>
2018-07-06 15:05:17 +0100

    delete

--- UmbracoCultures/App_Plugins/UmbracoCultures/UmbracoCultures.controller.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('umbraco').controller('UmbracoCulturesController', function ($route, $scope, $routeParams, $http, notificationsService, navigationService) {
-	$scope.cultures = "";
-	$http.get('/umbraco/backoffice/UmbracoCultures/UmbracoCultures/getCultures').then(function (response) {
-		console.log(response);
-		$scope.cultures = response.data;
-	}, function (error) { });
-	angular.element('input[name="UmbracoCultures"]').closest(".umb-property.ng-scope").hide();
-});
